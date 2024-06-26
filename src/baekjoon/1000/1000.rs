7300bc680956a5236947e3a1b31afb792a265ebf
seo-rii <me@seorii.page>
2024-06-26 11:17:12 +0900

    initial commit

---
+++ src/baekjoon/1000/1000.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let mut iter = input.split_whitespace();
+    let a: i32 = iter.next().unwrap().parse().unwrap();
+    let b: i32 = iter.next().unwrap().parse().unwrap();
+    println!("{}", a + b);
+}
